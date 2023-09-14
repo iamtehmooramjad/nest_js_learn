da0c77ae74b53db2dbb98e99cbdd5314e887f72d
Arif Ullah Khan <arifullah.khan@stackupsolutions.com>
2023-09-14 12:46:38 +0500

    Validation and error handling done, Validation Pipes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/tasks.dto';
@@ -25,12 +25,22 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    //try to get task
+
+    //if not found, throw an error 404 (not found)
+
+    //otherwise, return the found task
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with id ${id} not found!`);
+    }
+    return found;
   }
 
   deleteTaskById(id: string) {
-    const tasks: Task[] = this.tasks.filter((task) => task.id !== id);
-    this.tasks = tasks;
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
