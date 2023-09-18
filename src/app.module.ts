57a07b0720fa0ceb97af2268dea9c6171de5f080
Tehmoor Amjad <iamtehmooramjad@gmail.com>
2023-09-18 16:45:20 -0700

    Configurations and validation for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,47 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './tasks/task.entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './auth/user.entity';
+import { Task } from './tasks/task.entity';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      entities: [Task, User],
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
+
+    //To load module async, because this module depends on Config Module
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [Task, User],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'postgres',
+    //   database: 'task-management',
+    //   entities: [Task, User],
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
     AuthModule,
   ],
 })
